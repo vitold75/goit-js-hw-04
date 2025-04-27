743703b39307339a919d47a20a63ac4fd9b8b609
vito <vito333@ukr.net>
2025-04-27 23:06:30 +0300

    123

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,24 @@
-function slugify(title) {
-    const slug = (title.toLowerCase()).split(" ");
-    return slug.join("-");
+function isEnoughCapacity(products, containerSize) {
+    let total = 0;
+  const values = Object.values(products);
+  for (const value of values) {
+    total += value;
+  }
+    return (total <= containerSize);
 }
 
-console.log(slugify("Arrays for beginners"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
